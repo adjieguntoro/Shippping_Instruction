841cb213b5c2a90d855b05fad94e5e6f32972ecd
Adjie Guntoro <adjie.mytangsel@gmail.com>
2016-05-09 09:01:50 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://mytangsel.com/shippapp';
+$config['base_url'] = 'http://shippapp.azurewebsites.net';
 
 /*
 |--------------------------------------------------------------------------
